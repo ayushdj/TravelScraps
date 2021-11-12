ae8575e55f2cdf202538b0b67d969a507d6ccfa2
priyalpat721 <priyalpat721@gmail.com>
2021-11-12 12:13:15 -0500

    adding home screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+import {BrowserRouter, Route} from "react-router-dom";
+import HomeScreen from "./components/HomeScreen/HomeScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <div className="container">
+            <HomeScreen/>
+        </div>
 
+  );
+
+}
 export default App;
