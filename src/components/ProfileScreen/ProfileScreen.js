559f3c93eee380806636a0329342ccce096ca3f0
priyalpat721 <priyalpat721@gmail.com>
2021-11-15 11:59:41 -0500

    temp

--- src/components/ProfileScreen/ProfileScreen.js
+++ src/components/ProfileScreen/ProfileScreen.js
@@ -4,7 +4,6 @@ import NavigationSidebar from "../NavigationSidebar/NavigationSidebar.js";
 
 import ProfilePage from "./ProfilePage";
 import {useSelector} from "react-redux";
-import EditProfile from "./EditProfile";
 
 
 const ProfileScreen = () => {
